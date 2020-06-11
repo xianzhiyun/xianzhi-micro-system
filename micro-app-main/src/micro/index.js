05c195f0f4f9dff234ad4eca8aa3b631b18402ca
zhangle <zhangle@vtstar.net>
2020-06-12 06:34:26 +0800

    feat(mvp): 一个简单版mvp产品实现

--- micro-app-main/src/micro/index.js
+++ micro-app-main/src/micro/index.js
@@ -4,7 +4,22 @@ import {
   start
 } from 'qiankun'
 
-import apps from './apps' // 子应用注册信息
+// 子应用注册信息
+const apps = [
+  /**
+   * name: 微应用名称 - 具有唯一性，为了方便区分，采取和子应用名称
+   * entry: 微应用入口 - 通过该地址加载微应用
+   * container: 微应用挂载节点 - 微应用加载完成后将挂载在该节点上
+   * activeRule: 微应用触发的路由规则 - 触发路由规则后将加载该微应用
+   */
+  {
+    name: 'xianzhi-client',
+    entry: '//localhost:10200',
+    container: '#frame',
+    activeRule: '/client'
+  }
+]
+
 /**
  * 注册子应用
  * 第一个参数 - 子应用的注册信息
