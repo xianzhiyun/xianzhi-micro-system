596bf17b06af4a76324ecf83d26a9fcb6badf054
zhangle <zhangle@vtstar.net>
2020-06-13 14:56:10 +0800

    feat(mvp): 实现通讯

--- xianzhi-client/src/views/profile/index.vue
+++ xianzhi-client/src/views/profile/index.vue
@@ -1,15 +1,26 @@
 <template>
   <div class="app-container">
     <h1>个人中心页面</h1>
+    <div>token: {{ token || '暂无' }}</div>
   </div>
 </template>
 
 <script>
 
+import actions from '@/shared/actions'
 export default {
   name: 'Profile',
   data() {
-    return {}
+    return {
+      token: null
+    }
+  },
+  mounted() {
+    actions.onGlobalStateChange(state => {
+      const { token } = state
+      this.token = token
+      console.log(`%c 子应用获取到了`, 'font-size: 16px; font-weight: bold;color:green', token)
+    }, true)
   }
 }
 </script>
